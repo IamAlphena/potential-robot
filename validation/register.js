bf513a6b224735aa6b3bc5e44b41bd5702ddbce7
Alphena <alphena@gmail.com>
2021-05-24 23:40:27 -0700

    login base

---
+++ validation/register.js
@@ -0,0 +1,43 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    //Convert Empty fields to an empty string before running validation checks
+    data.name = !isEmpty(data.name) ? data.name : "";
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+    //Check Name
+    if (Validator.isEmpty(data.name)) {
+        errors.name = "Name is required"
+    }
+
+    //Check Email
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email is required"
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid"
+    }
+
+    //Check Password
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = "Confirm Password is required";
+    }
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = "Password must be at least 6 characters (to a max of 30)";
+    }
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match";
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+
+}
