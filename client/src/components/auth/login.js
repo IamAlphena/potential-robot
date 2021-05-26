7dadfa3c4351b14c5463f13b67c3fa4cb9585fe5
Alphena <alphena@gmail.com>
2021-05-26 02:04:14 -0700

    base structure, still troubleshooting

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import { Form, Button, Icon } from "react-bulma-components";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+
 class Login extends Component {
   constructor() {
     super();
@@ -9,56 +15,107 @@ class Login extends Component {
       errors: {}
     };
   }
-onChange = e => {
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); // push user to dashboard when they login
+    }
+if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
-onSubmit = e => {
+
+  onSubmit = e => {
     e.preventDefault();
-const userData = {
+
+    const userData = {
       email: this.state.email,
       password: this.state.password
     };
-console.log(userData);
+
+    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
   };
-render() {
+
+  render() {
     const { errors } = this.state;
-return (
-    <>
-    <Form.Field >
-      <Form.Label>Email</Form.Label>
-      <Form.Control>
-        <Form.Input
-        onChange={this.onChange}
-        value={this.state.email}
-        error={errors.email}
-        id="email"
-        type="email"
-        />
-      </Form.Control>
-    </Form.Field>
-    <Form.Field>
-      <Form.Label>Password</Form.Label>
-      <Form.Control>
-        <Form.Input
-           onChange={this.onChange}
-           value={this.state.password}
-           error={errors.password}
-           id="password"
-           type="password"
-        />
-      </Form.Control>
-    </Form.Field>
-    <div className="spacer"></div>
-    <Button.Group>
-      <div className="btn">
-      <Button fullwidth rounded color="link" onClick={handleSubmit}>
-        Login
+
+    return (
+      <>
+        <Form.Field >
+          <Form.Label>Email</Form.Label>
+          <Form.Control>
+            <Form.Input
+              onChange={this.onChange}
+              value={this.state.email}
+              error={errors.email}
+              id="email"
+              type="email"
+              className={classnames("", {
+                invalid: errors.email || errors.emailnotfound
+              })}
+            />
+          </Form.Control>
+          <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
+        </Form.Field>
+        <Form.Field>
+          <Form.Label>Password</Form.Label>
+          <Form.Control>
+            <Form.Input
+              onChange={this.onChange}
+              value={this.state.password}
+              error={errors.password}
+              id="password"
+              type="password"
+              className={classnames("", {
+                invalid: errors.password || errors.passwordincorrect
+              })}
+            />
+          </Form.Control>
+          <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
+        </Form.Field>
+        <div className="spacer"></div>
+        <Button.Group>
+          <div className="btn">
+            <Button fullwidth rounded color="link"
+            //  onClick={handleSubmit}
+            >
+              Login
       </Button>
-      </div>
-    </Button.Group>
-  </>
-);
-}
+          </div>
+        </Button.Group>
+      </>
+    );
+  }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,6 +1,11 @@
 import React, { Component, useState } from "react";
 import axios from "axios";
 import { Form, Button, Icon } from "react-bulma-components";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
+import { withRouter } from "react-router-dom";
 
 class Register extends Component {
     constructor() {
@@ -13,6 +18,22 @@ class Register extends Component {
           errors: {}
         };
       }
+
+      componentDidMount() {
+        // If logged in and user navigates to Register page, should redirect them to dashboard
+        if (this.props.auth.isAuthenticated) {
+          this.props.history.push("/dashboard");
+        }
+      }
+
+      componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+          this.setState({
+            errors: nextProps.errors
+          });
+        }
+      }
+
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value });
       };
@@ -24,8 +45,10 @@ class Register extends Component {
           password: this.state.password,
           password2: this.state.password2
         };
-    console.log(newUser);
-      };
+        
+    this.props.registerUser(newUser, this.props.history); 
+  }; 
+
     render() {
         const { errors } = this.state;
 
@@ -40,8 +63,13 @@ class Register extends Component {
           error={errors.name}
           id="name"
           type="text"
+          className={classnames("", {
+            invalid: errors.name
+          })}
           />
+
         </Form.Control>
+        <span className="red-text">{errors.name}</span>
       </Form.Field>
       <Form.Field>
         <Form.Label>Email</Form.Label>
@@ -52,8 +80,12 @@ class Register extends Component {
            error={errors.email}
            id="email"
            type="email"
+           className={classnames("", {
+            invalid: errors.email
+          })}
           />
         </Form.Control>
+        <span className="red-text">{errors.email}</span>
       </Form.Field>
       <Form.Field>
         <Form.Label>Password</Form.Label>
@@ -64,8 +96,12 @@ class Register extends Component {
              error={errors.password}
              id="password"
              type="password"
+             className={classnames("", {
+              invalid: errors.password
+            })}
           />
         </Form.Control>
+        <span className="red-text">{errors.password}</span>
       </Form.Field>
       <Form.Field>
         <Form.Label>Confirm Password</Form.Label>
@@ -76,12 +112,18 @@ class Register extends Component {
             error={errors.password2}
             id="password2"
             type="password"
+            className={classnames("", {
+              invalid: errors.password2
+            })}
           />
         </Form.Control>
+        <span className="red-text">{errors.password2}</span>
       </Form.Field>
       <Button.Group>
         <div className="btn">
-        <Button fullwidth rounded color="link" onClick={handleSubmit}>
+        <Button fullwidth rounded color="link" 
+        // onClick={handleSubmit}
+        >
           Register
         </Button>
         </div>
@@ -91,4 +133,16 @@ class Register extends Component {
 }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
